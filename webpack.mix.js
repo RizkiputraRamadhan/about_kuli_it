cc83e9a2522c1a997611ea144a12b4d15ff67f45
rizki kuli IT <rizkikuliit@gmail.com>
2025-02-28 15:46:37 +0700

    update : rollback mix production

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,23 @@
 const mix = require("laravel-mix");
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/css/app.scss', 'public/css') // Ubah path ini jika app.scss ada di resources/css
-   .version();
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
 
-mix.copyDirectory('resources/client/dist', 'public/client/dist');
+// mix.js("resources/js/app.js", "public/js").postCss(
+//     "resources/css/app.css",
+//     "public/css",
+//     [
+//         //
+//     ]
+// );
 
 let plugins = [
     "bootstrap",
